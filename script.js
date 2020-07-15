703dd106d3dd5327de2d8fab6c259c014f0df848
Anna <anna_agabekyan_anna@mail.ru>
2020-07-15 08:34:54 +0400

    fix

--- script.js
+++ script.js
@@ -3,18 +3,42 @@ let columnIdCounter = 4
 
 document
     .querySelectorAll('.column')
-    .forEach(columnElement=>{
-        const spanAction_addNote = columnElement.querySelector('[data-action-addNote]')
+    .forEach(columnProcess)
 
-        spanAction_addNote.addEventListener('click', function () {
-            const noteElement = document.createElement('div')
-            noteElement.classList.add('note')
-            console.log('click')
-            noteElement.setAttribute('draggable', 'true')
-            noteElement.setAttribute('id', noteIdCounter)
-            noteIdCounter++
-            columnElement.querySelector('[data-notes]').append(noteElement)
-          //  <div class="note" draggable="true" data-note-id="7">Купить собачий корм.</div>
+document
+    .querySelectorAll('[data-action-addColumn]')
+    .addEventListener('click', function(event){
+      const columnElement = document.createElement('div')
+      columnElement.classList.add('column')
+      columnElement.setAttribute('draggable', 'true')
+      columnElement.setAttribute('data-column-id', columnIdCounter)
 
-            })
+      columnElement.innerHTML=`
+      <p class="column-header" contenteditable="true">В плане</p>
+					<div data-notes></div>
+					<p class="column-footer">
+						<span data-action-addNote class="action">+ Добавить карточку</span>
+          </p>`
+          
+          columnIdCounter++
+
+          document.querySelector('.columns').append(columnElement)
+
+          columnProcess(columnElement)
     })
+
+function columnProcess (columnElement) {
+      const spanAction_addNote = columnElement.querySelector('[data-action-addNote]')
+
+  spanAction_addNote.addEventListener('click', function () {
+    const noteElement = document.createElement('div')
+    noteElement.classList.add('note')
+    noteElement.setAttribute('draggable', 'true')
+    noteElement.setAttribute('data-note-id', noteIdCounter)
+
+    noteIdCounter++
+
+    columnElement.querySelector('[data-notes]').append(noteElement)
+
+    })
+  }
